0543ce6136701c1a7e4d0d30fc28d397ca6ca3da
GOKUL SONA <142295118+gokulsona@users.noreply.github.com>
2023-11-17 22:25:32 +0530

    Update StackUsingArray.c

--- DataStructure_Lab/StackUsingArray.c
+++ DataStructure_Lab/StackUsingArray.c
@@ -1,19 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include<stdio.h>
 #include<stdlib.h>
 int top=-1,size=5,arr[5];
